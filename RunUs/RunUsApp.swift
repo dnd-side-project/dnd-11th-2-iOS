c2877aa64cf7d6896c4ed2a756c91da402e14e85
sladuf <04fud27@gmail.com>
2024-07-19 14:54:48 +0900

    Initial Commit

---
+++ RunUs/RunUsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunUsApp.swift
+//  RunUs
+//
+//  Created by Ryeong on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct RunUsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
