dd28f953c710749cc0ea9766d88e4a4e91252120
sladuf <04fud27@gmail.com>
2024-07-24 15:06:08 +0900

    feat #1: 서버 base 통신 객체 구현

---
+++ RunUs/Data/Server/ServerResponse.swift
@@ -0,0 +1,20 @@
+//
+//  ServerResponse.swift
+//  RunUs
+//
+//  Created by Ryeong on 7/23/24.
+//
+
+import Foundation
+
+struct ServerResponse<T: Decodable>: Decodable {
+    let success: Bool
+    let data: T?
+    let error: ServerError?
+}
+
+struct ServerError: Decodable {
+    let statusCode: Int
+    let code: String
+    let message: String
+}
