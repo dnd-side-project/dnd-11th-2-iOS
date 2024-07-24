23b243bfb80e9827f915ab7abcb5caf60659e961
sladuf <04fud27@gmail.com>
2024-07-24 15:06:37 +0900

    feat #1: Server 통신 객체 구현

---
+++ RunUs/Data/Server/ServerNetwork.swift
@@ -0,0 +1,45 @@
+//
+//  ServerNetwork.swift
+//  RunUs
+//
+//  Created by Ryeong on 7/24/24.
+//
+
+import Foundation
+import Combine
+
+class ServerNetwork {
+    static let shared = ServerNetwork()
+    
+    private init() { }
+    
+    func request<T: Decodable>(_ endpoint: ServerEndpoint) -> AnyPublisher<T, NetworkError> {
+        NetworkService.shared.request(endpoint)
+            .tryMap { (response: ServerResponse<T>) in
+                if let data = response.data, response.success {
+                    return data
+                } else if let error = response.error {
+                    throw NetworkError.server(code: error.statusCode)
+                } else {
+                    throw NetworkError.parse
+                }
+            }
+            .mapError{ _ in NetworkError.unknown }
+            .eraseToAnyPublisher()
+    }
+    
+    func request(_ endpoint: ServerEndpoint) -> AnyPublisher<Void, NetworkError> {
+        NetworkService.shared.request(endpoint)
+            .tryMap { (response: ServerResponse<Bool>) in
+                if response.success {
+                    return
+                } else if let error = response.error {
+                    throw NetworkError.server(code: error.statusCode)
+                } else {
+                    throw NetworkError.parse
+                }
+            }
+            .mapError{ _ in NetworkError.unknown }
+            .eraseToAnyPublisher()
+    }
+}
