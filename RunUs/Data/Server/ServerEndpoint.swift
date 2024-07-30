01c9ae0a09a72fd0355a1d01667b6bc0e83e7465
sladuf <04fud27@gmail.com>
2024-07-30 19:47:04 +0900

    feat #8: server test API 적용

--- RunUs/Data/Server/ServerEndpoint.swift
+++ RunUs/Data/Server/ServerEndpoint.swift
@@ -7,14 +7,42 @@
 
 import Foundation
 
-//TODO: Server 나오면 수정
 enum ServerEndpoint: NetworkEndpoint {
-    case test
+    case testRequest(string: String)
+    case testResponse
+    case testError
     
-    var baseURL: URL? { nil }
-    var path: String { "" }
-    var method: NetworkMethod { .get }
-    var parameters: [URLQueryItem]? { nil }
+    var baseURL: URL? { URL(string: "https://api.runus.site") }
+    
+    enum APIversion {
+        static let v1 = "/api/v1"
+    }
+    
+    var path: String {
+        switch self {
+        case .testRequest:
+            return APIversion.v1 + "/examples/input"
+        case .testResponse:
+            return APIversion.v1 + "/examples/empty"
+        case .testError:
+            return APIversion.v1 + "/examples/errors"
+        }
+    }
+    
+    var method: NetworkMethod {
+        switch self {
+        case .testRequest, .testResponse, .testError:
+            return .get
+        }
+    }
+    var parameters: [URLQueryItem]? {
+        switch self {
+        case .testRequest(let string):
+            return [.init(name: "input", value: string)]
+        case .testResponse, .testError:
+            return nil
+        }
+    }
     var header: [String : String]? { nil }
     var body: Data? { nil }
 }
