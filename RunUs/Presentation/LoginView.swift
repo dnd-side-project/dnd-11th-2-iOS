d4ed6144e801cdb15adb76d9d2ac0b0d4d2195e7
seungyooooong <chadange@naver.com>
2024-08-01 12:24:39 +0900

    feat #3: 수정된 버튼 디자인 값(width, padding 등) 반영

--- RunUs/Presentation/LoginView.swift
+++ RunUs/Presentation/LoginView.swift
@@ -9,40 +9,44 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        VStack {
-            HStack {
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("Run Earth With Runus!")
-                        .font(.custom(Fonts.pretendardMedium, size: 24))
-                    Text("런어스랑 지구한바퀴 뛰어보기")
-                        .font(.custom(Fonts.pretendardExtraBold, size: 24))
+        GeometryReader { geometry in
+            VStack {
+                HStack {
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Run Earth With Runus!")
+                            .font(.custom(Fonts.pretendardMedium, size: 24))
+                        Text("런어스랑 지구한바퀴 뛰어보기")
+                            .font(.custom(Fonts.pretendardExtraBold, size: 24))
+                    }
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 8)
+                    Spacer()
                 }
-                .padding(.horizontal, 16)
-                .padding(.vertical, 8)
+                .foregroundStyle(.white)
+                .padding(.top, 160)
                 Spacer()
+                Button {
+                    
+                } label: {
+                    Label(  // 추후 컴포넌트화하기
+                        title: {
+                            Text("Apple ID로 시작하기")
+                                .font(.custom(Fonts.pretendardVariable, size: 16))
+                        },
+                        icon: { Image(systemName: "apple.logo") }
+                    )
+                }
+                .foregroundColor(.black)
+                .background {
+                    Rectangle()
+                        .fill(.white)
+                        .cornerRadius(24)
+                        .padding(.horizontal, 16)
+                        .padding(.vertical, 8)
+                        .frame(width: geometry.size.width, height: 48 + 16)
+                }
+                .padding(.bottom, 67)
             }
-            .foregroundStyle(.white)
-            .padding(.top, 160)
-            Spacer()
-            Button {
-                
-            } label: {
-                Label(  // 추후 컴포넌트화하기
-                    title: {
-                        Text("Apple ID로 시작하기")
-                            .font(.custom(Fonts.pretendardVariable, size: 16))
-                    },
-                    icon: { Image(systemName: "apple.logo") }
-                )
-            }
-            .foregroundColor(.black)
-            .background {
-                Rectangle()
-                    .fill(.white)
-                    .frame(width: 320, height: 48)
-                    .cornerRadius(24)
-            }
-            .padding(.bottom, 75)
         }
         .ignoresSafeArea()
         .background(Color("BackgroundColor"))
