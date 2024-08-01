b8c09c260b150994810bf3068fbfdc5a012cd90d
seungyooooong <chadange@naver.com>
2024-08-01 12:02:54 +0900

    feat #3: 불필요한 코드 삭제 및 Singing & Capabilities 추가

--- RunUs/Presentation/LoginView.swift
+++ RunUs/Presentation/LoginView.swift
@@ -9,43 +9,40 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                HStack {
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text("Run Earth With Runus!")
-                            .font(.custom(Fonts.pretendardMedium, size: 24))
-                        Text("런어스랑 지구한바퀴 뛰어보기")
-                            .font(.custom(Fonts.pretendardExtraBold, size: 24))
-                    }
-                    .padding(.horizontal, 16)
-                    .padding(.vertical, 8)
-                    Spacer()
+        VStack {
+            HStack {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Run Earth With Runus!")
+                        .font(.custom(Fonts.pretendardMedium, size: 24))
+                    Text("런어스랑 지구한바퀴 뛰어보기")
+                        .font(.custom(Fonts.pretendardExtraBold, size: 24))
                 }
-                .foregroundStyle(.white)
-                .padding(.top, 160)
+                .padding(.horizontal, 16)
+                .padding(.vertical, 8)
                 Spacer()
-                Button {
-                    
-                } label: {
-                    Label(  // 추후 컴포넌트화하기
-                        title: {
-                            Text("Apple ID로 시작하기")
-                                .font(.custom(Fonts.pretendardVariable, size: 16))
-                        },
-                        icon: { Image(systemName: "apple.logo") }
-                    )
-                }
-                .foregroundColor(.black)
-                .background {
-                    Rectangle()
-                        .fill(.white)
-                        .frame(width: 320, height: 48)
-                        .cornerRadius(24)
-                }
-                .padding(.bottom, 75)
             }
-            .frame(width: geometry.size.width, height: geometry.size.height)
+            .foregroundStyle(.white)
+            .padding(.top, 160)
+            Spacer()
+            Button {
+                
+            } label: {
+                Label(  // 추후 컴포넌트화하기
+                    title: {
+                        Text("Apple ID로 시작하기")
+                            .font(.custom(Fonts.pretendardVariable, size: 16))
+                    },
+                    icon: { Image(systemName: "apple.logo") }
+                )
+            }
+            .foregroundColor(.black)
+            .background {
+                Rectangle()
+                    .fill(.white)
+                    .frame(width: 320, height: 48)
+                    .cornerRadius(24)
+            }
+            .padding(.bottom, 75)
         }
         .ignoresSafeArea()
         .background(Color("BackgroundColor"))
