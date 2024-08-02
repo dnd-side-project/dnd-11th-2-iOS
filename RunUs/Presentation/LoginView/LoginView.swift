4f26ffdece61c514d228e3079a8d97d7427a7862
seungyooooong <chadange@naver.com>
2024-08-02 15:21:22 +0900

    feat #19: 최상단뷰에 배경색 추가 및 geometry 삭제

--- RunUs/Presentation/LoginView/LoginView.swift
+++ RunUs/Presentation/LoginView/LoginView.swift
@@ -12,49 +12,48 @@ struct LoginView: View {
     let store: StoreOf<LoginStore>
     
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                HStack {
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text("Run Earth With Runus!")
-                            .font(.custom(Fonts.pretendardMedium, size: 24))
-                        Text("런어스랑 지구한바퀴 뛰어보기")
-                            .font(.custom(Fonts.pretendardExtraBold, size: 24))
-                    }
-                    .padding(.horizontal, 16)
-                    .padding(.vertical, 8)
-                    Spacer()
+        VStack {
+            HStack {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Run Earth With Runus!")
+                        .font(.custom(Fonts.pretendardMedium, size: 24))
+                    Text("런어스랑 지구한바퀴 뛰어보기")
+                        .font(.custom(Fonts.pretendardExtraBold, size: 24))
                 }
-                .foregroundStyle(.white)
-                .padding(.top, 160)
+                .padding(.horizontal, 16)
+                .padding(.vertical, 8)
                 Spacer()
-                WithViewStore(store, observe: { $0 }) { viewStore in
-                    Button {
-                        viewStore.send(.doAppleLogin)
-                    } label: {
-                        Label(  // 추후 컴포넌트화하기
-                            title: {
-                                Text("Apple ID로 시작하기")
-                                    .font(.custom(Fonts.pretendardVariable, size: 16))
-                            },
-                            icon: { Image(systemName: "apple.logo") }
-                        )
-                    }
-                    .foregroundColor(.black)
-                    .background {
-                        Rectangle()
-                            .fill(.white)
-                            .cornerRadius(24)
-                            .padding(.horizontal, 16)
-                            .padding(.vertical, 8)
-                            .frame(width: geometry.size.width, height: 48 + 16)
-                    }
-                    .padding(.bottom, 67)
+            }
+            .foregroundStyle(.white)
+            .padding(.top, 160)
+            Spacer()
+            WithViewStore(store, observe: { $0 }) { viewStore in
+                Button {
+                    viewStore.send(.doAppleLogin)
+                } label: {
+                    Label(  // 추후 컴포넌트화하기
+                        title: {
+                            Text("Apple ID로 시작하기")
+                                .font(.custom(Fonts.pretendardVariable, size: 16))
+                        },
+                        icon: { Image(systemName: "apple.logo") }
+                    )
                 }
+                .frame(maxWidth: .infinity)
+                .foregroundColor(.black)
+                .background {
+                    Rectangle()
+                        .fill(.white)
+                        .cornerRadius(24)
+                        .padding(.horizontal, 16)
+                        .padding(.vertical, 8)
+                        .frame(height: 48 + 16)
+                        .frame(maxWidth: .infinity)
+                }
+                .padding(.bottom, 67)
             }
         }
         .ignoresSafeArea()
-        .background(Color("BackgroundColor"))
     }
 }
 
@@ -65,4 +64,5 @@ struct LoginView: View {
             reducer: { LoginStore() }
         )
     )
+    .background(Colors.background)
 }
