1392ac8d5d5cd6103709bed3b8deb0f50c08117c
sladuf <04fud27@gmail.com>
2024-08-06 00:15:44 +0900

    feat #7: 홈화면 임시 변경

--- RunUs/Home/HomeView.swift
+++ RunUs/Home/HomeView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("HomeView")
+        //FIXME: 홈화면 디자인 나오면 수정
+        NavigationView{
+            NavigationLink {
+                RunAloneHomeView()
+            } label: {
+                Text("혼자뛰기")
+            }
+        }
     }
 }
 
