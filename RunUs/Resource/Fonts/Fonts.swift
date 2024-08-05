59b91fdb6303b4214a61a166b6ea2f86d4744d2b
seungyooooong <chadange@naver.com>
2024-08-05 13:44:44 +0900

    feat #10: 디자인 시스템 색상 & 아이콘 적용

--- RunUs/Resource/Fonts/Fonts.swift
+++ RunUs/Resource/Fonts/Fonts.swift
@@ -9,7 +9,15 @@ import Foundation
 import SwiftUI
 
 enum Fonts {
-    static let pretendardMedium = "Pretendard-Mideum"
+    // pretendard
+    static let pretendardBlack = "Pretendard-Black"
+    static let pretendardBold = "Pretendard-Bold"
     static let pretendardExtraBold = "Pretendard-ExtraBold"
+    static let pretendardExtraLight = "Pretendard-ExtraLight"
+    static let pretendardLight = "Pretendard-Light"
+    static let pretendardMedium = "Pretendard-Mideum"
+    static let pretendardRegular = "Pretendard-Regular"
+    static let pretendardSemiBold = "Pretendard-SemiBold"
+    static let pretendardThin = "Pretendard-Thin"
     static let pretendardVariable = "PretendardVariable"
 }
---
+++ RunUs/Resource/Icons/Icons.swift
@@ -0,0 +1,30 @@
+//
+//  Icons.swift
+//  RunUs
+//
+//  Created by seungyooooong on 8/5/24.
+//
+
+import Foundation
+
+enum Icons {
+    // green icon
+    static let home = "home"
+    static let chart = "chart"
+    
+    // chevron
+    static let chevronUp = "chevron.up"
+    static let chevronDown = "chevron.down"
+    static let chevronLeft = "chevron.left"
+    static let chevronRight = "chevron.right"
+    
+    // menu
+    static let hambuger = "hambuger"
+    static let kebab = "kebab"
+    static let meatball = "meatball"
+    static let xmark = "xmark"
+    
+    // refresh
+    static let refresh = "refresh"
+    static let refreshCircle = "refresh.circle"
+}
