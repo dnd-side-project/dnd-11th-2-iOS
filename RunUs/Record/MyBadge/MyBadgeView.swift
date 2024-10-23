2a1fe2834bf79ec6d01bf4f0984d17f3eefdb3c7
seungyooooong <chadange@naver.com>
2024-10-23 16:24:18 +0900

    feat #80: getBadgeLists API 연동

--- RunUs/Record/MyBadge/MyBadgeView.swift
+++ RunUs/Record/MyBadge/MyBadgeView.swift
@@ -6,8 +6,14 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct MyBadgeView: View {
+    @State var store: StoreOf<MyBadgeStore> = Store(
+        initialState: MyBadgeStore.State(),
+        reducer: { MyBadgeStore() }
+    )
+    
     var body: some View {
         ViewThatFits(in: .vertical) {
             myBadgeView
@@ -18,7 +24,7 @@ struct MyBadgeView: View {
         .padding(.top, 1)   // MARK: ViewThatFits에서 ScrollView를 사용하면 SafeArea를 유지하기 위해 필요
         .background(Color.background)
         .onAppear {
-            
+            store.send(.onAppear)
         }
     }
 }
@@ -28,17 +34,17 @@ extension MyBadgeView {
         VStack(spacing: 0) {
             RUNavigationBar(buttonType: .back, title: "나의뱃지")
             RUTitle(text: "신규 뱃지")
-            RUBadgeList(badges: [])
+            RUBadgeList(badges: store.badgeLists.recencyBadges)
             RUTitle(text: "개인 기록")
-            RUBadgeList(badges: [])
+            RUBadgeList(badges: store.badgeLists.personalBadges)
             RUTitle(text: "러닝 거리")
-            RUBadgeList(badges: [])
+            RUBadgeList(badges: store.badgeLists.distanceBadges)
             RUTitle(text: "streak")
-            RUBadgeList(badges: [])
+            RUBadgeList(badges: store.badgeLists.streakBadges)
             RUTitle(text: "duration")
-            RUBadgeList(badges: [])
+            RUBadgeList(badges: store.badgeLists.durationBadges)
             RUTitle(text: "level")
-            RUBadgeList(badges: [])
+            RUBadgeList(badges: store.badgeLists.levelBadges)
             Spacer()
         }
         .padding(.horizontal, Paddings.outsideHorizontalPadding)
