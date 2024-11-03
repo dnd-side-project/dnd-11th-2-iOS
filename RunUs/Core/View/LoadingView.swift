f1b6ca6b87e6176b818da0087cdcf2724382b159
seungyooooong <chadange@naver.com>
2024-11-03 12:29:52 +0900

    feat #45: 로딩 뷰 UX 개선

--- RunUs/Core/View/LoadingView.swift
+++ RunUs/Core/View/LoadingView.swift
@@ -8,10 +8,24 @@
 import SwiftUI
 
 struct LoadingView: View {
+    @State var isShowLoading = false
+    @State var timer: Timer?
+    
     var body: some View {
         ProgressView()
+            .opacity(isShowLoading ? 1 : 0)
             .frame(maxWidth:. infinity, maxHeight: .infinity)
-            .background(Color.background.opacity(0.2))
+            .background(Color.background.opacity(isShowLoading ? 0.5 : 0.01))
+            .onAppear {
+                timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { timer in
+                    isShowLoading = true
+                }
+            }
+            .onDisappear {
+                timer?.invalidate()
+                timer = nil
+                isShowLoading = false
+            }
     }
 }
 
