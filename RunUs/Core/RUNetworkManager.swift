17a3e81b6de2d614d803b7d2e3038141ff361878
seungyooooong <chadange@naver.com>
2024-11-22 11:24:45 +0900

    feat #103: 회원가입 에러 케이스 세분화

--- RunUs/Core/RUNetworkManager.swift
+++ RunUs/Core/RUNetworkManager.swift
@@ -21,6 +21,8 @@ struct RUNetworkManager {
             if error.isLoginError {
                 UserDefaultManager.logout()
                 AlertManager.shared.showAlert(title: "런어스 서비스 이용을 위해서는\n회원 가입이 필요합니다.", isSingleButtonAlert: true)
+            } else if error.needRevokeError {
+                AlertManager.shared.showAlert(title: "런어스 서비스 재이용을 위해서는\n설정 > Apple 계정 > Apple로 로그인 > RunUs > 삭제\n작업을 먼저 수행해주세요.", isSingleButtonAlert: true)
             } else {
                 await showNetworkAlert(retryAction)
             }
